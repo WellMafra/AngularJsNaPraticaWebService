c10403b399f2fd921be4ea2670d6537708ae3826
Wellington <well_wim@hotmail.com>
2014-07-03 18:25:55 -0300

    Fixed name of field.

--- src/main/java/org/example/salesserver/model/Customer.java
+++ src/main/java/org/example/salesserver/model/Customer.java
@@ -11,172 +11,145 @@ import javax.xml.bind.annotation.XmlRootElement;
 @Entity
 @Table(name = "customers")
 @XmlRootElement
-public class Customer implements Serializable
-{
+public class Customer implements Serializable {
 
-   private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-   @Id
-   @Column(nullable = false, updatable = false, length = 5)
-   private String customerID;
-   @Column
-   private String companyName;
-   @Column
-   private String contactName;
-   @Column
-   private String contactTitle;
-   @Column
-   private String address;
-   @Column
-   private String cty;
-   @Column
-   private String region;
-   @Column
-   private String postalCode;
-   @Column
-   private String country;
-   @Column
-   private String phone;
-   @Column
-   private String fax;
+	@Id
+	@Column(nullable = false, updatable = false, length = 5)
+	private String customerID;
+	@Column
+	private String companyName;
+	@Column
+	private String contactName;
+	@Column
+	private String contactTitle;
+	@Column
+	private String address;
+	@Column
+	private String city;
+	@Column
+	private String region;
+	@Column
+	private String postalCode;
+	@Column
+	private String country;
+	@Column
+	private String phone;
+	@Column
+	private String fax;
 
-   public String getCustomerID()
-   {
-      return customerID;
-   }
+	public String getCustomerID() {
+		return customerID;
+	}
 
-   public void setCustomerID(String customerID)
-   {
-      this.customerID = customerID;
-   }
+	public void setCustomerID(String customerID) {
+		this.customerID = customerID;
+	}
 
-   public String getCompanyName()
-   {
-      return companyName;
-   }
+	public String getCompanyName() {
+		return companyName;
+	}
 
-   public void setCompanyName(String companyName)
-   {
-      this.companyName = companyName;
-   }
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
 
-   public String getContactName()
-   {
-      return contactName;
-   }
+	public String getContactName() {
+		return contactName;
+	}
 
-   public void setContactName(String contactName)
-   {
-      this.contactName = contactName;
-   }
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
 
-   public String getContactTitle()
-   {
-      return contactTitle;
-   }
+	public String getContactTitle() {
+		return contactTitle;
+	}
 
-   public void setContactTitle(String contactTitle)
-   {
-      this.contactTitle = contactTitle;
-   }
+	public void setContactTitle(String contactTitle) {
+		this.contactTitle = contactTitle;
+	}
 
-   public String getAddress()
-   {
-      return address;
-   }
+	public String getAddress() {
+		return address;
+	}
 
-   public void setAddress(String address)
-   {
-      this.address = address;
-   }
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
-   public String getCty()
-   {
-      return cty;
-   }
+	public String getCity() {
+		return city;
+	}
 
-   public void setCty(String cty)
-   {
-      this.cty = cty;
-   }
+	public void setCty(String city) {
+		this.city = city;
+	}
 
-   public String getRegion()
-   {
-      return region;
-   }
+	public String getRegion() {
+		return region;
+	}
 
-   public void setRegion(String region)
-   {
-      this.region = region;
-   }
+	public void setRegion(String region) {
+		this.region = region;
+	}
 
-   public String getPostalCode()
-   {
-      return postalCode;
-   }
+	public String getPostalCode() {
+		return postalCode;
+	}
 
-   public void setPostalCode(String postalCode)
-   {
-      this.postalCode = postalCode;
-   }
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
 
-   public String getCountry()
-   {
-      return country;
-   }
+	public String getCountry() {
+		return country;
+	}
 
-   public void setCountry(String country)
-   {
-      this.country = country;
-   }
+	public void setCountry(String country) {
+		this.country = country;
+	}
 
-   public String getPhone()
-   {
-      return phone;
-   }
+	public String getPhone() {
+		return phone;
+	}
 
-   public void setPhone(String phone)
-   {
-      this.phone = phone;
-   }
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
-   public String getFax()
-   {
-      return fax;
-   }
+	public String getFax() {
+		return fax;
+	}
 
-   public void setFax(String fax)
-   {
-      this.fax = fax;
-   }
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
 
-   @Override
-   public int hashCode()
-   {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ((customerID == null) ? 0 : customerID.hashCode());
-      return result;
-   }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((customerID == null) ? 0 : customerID.hashCode());
+		return result;
+	}
 
-   @Override
-   public boolean equals(Object obj)
-   {
-      if (this == obj)
-         return true;
-      if (obj == null)
-         return false;
-      if (getClass() != obj.getClass())
-         return false;
-      Customer other = (Customer) obj;
-      if (customerID == null)
-      {
-         if (other.customerID != null)
-            return false;
-      }
-      else if (!customerID.equals(other.customerID))
-         return false;
-      return true;
-   }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (customerID == null) {
+			if (other.customerID != null)
+				return false;
+		} else if (!customerID.equals(other.customerID))
+			return false;
+		return true;
+	}
 
 }
